79a4baa516f41d092b7ef8e5fc1167dfeff0bf68
brayan-murcia <murciabrayan324@gmail.com>
2025-06-11 14:48:26 -0500

    Add event listener for selecting mascota and update button IDs in Mokepon game

---
+++ Mokepon/js/Mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador() {
+    alert("¡Has seleccionado tu mascota!");
+}
+
+let botonSeleccionarMascota = document.getElementById('boton-seleccionar-mascota')
+botonSeleccionarMascota.addEventListener('click', seleccionarMascotaJugador)
