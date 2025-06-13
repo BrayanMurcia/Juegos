3608dadb10d3199acf3fc46cb1cc47f99daf0bba
brayan-murcia <murciabrayan324@gmail.com>
2025-06-12 19:53:15 -0500

    Improve mascota selection alerts in Mokepon game

--- Mokepon/js/Mokepon.js
+++ Mokepon/js/Mokepon.js
@@ -4,7 +4,28 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-    alert("¡Has seleccionado tu mascota!");
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+    let inputLangostelvis = document.getElementById("langostelvis")
+    let inputTucapalma = document.getElementById("tucapalma")
+    let inputPydos = document.getElementById("pydos")
+
+    if (inputHipodoge.checked) {
+        alert("Has seleccionado a Hipodoge")
+    } else if (inputCapipepo.checked) {
+        alert("Has seleccionado a Capipepo")
+    } else if (inputRatigueya.checked) {
+        alert("Has seleccionado a Ratigueya")
+    } else if (inputLangostelvis.checked) {
+        alert("Has seleccionado a Langostelvis")
+    } else if (inputTucapalma.checked) {
+        alert("Has seleccionado a Tucapalma")
+    } else if (inputPydos.checked) {
+        alert("Has seleccionado a Pydos")
+    } else {
+        alert("Selecciona una mascota")
+    }
 }
 
 window.addEventListener("load", iniciarJuego)
